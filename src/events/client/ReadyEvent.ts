e84bb7579c9000df7f9e594ce7a3b6c29c96aa0e
Milo123459 <milo5huntington@gmail.com>
2020-10-12 17:56:07 +0100

    set status

--- src/events/client/ReadyEvent.ts
+++ src/events/client/ReadyEvent.ts
@@ -2,4 +2,5 @@ import { RunFunction } from '../../interfaces/Event';
 export const name: string = 'ready';
 export const run: RunFunction = async (client) => {
 	client.logger.success(`${client.user.tag} is now online!`);
+	await client.user.setActivity(`sp!help | 👦 Spencer`, { type: 'WATCHING' })
 };
