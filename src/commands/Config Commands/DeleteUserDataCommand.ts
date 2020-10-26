c4bba2f214f463fdf95a88434e12ad1b608c5b6f
Milo123459 <milo5huntington@gmail.com>
2020-10-26 15:56:54 +0000

    feat(Commands): add delete userdata command for ecnomomy system

---
+++ src/commands/Config Commands/DeleteUserDataCommand.ts
@@ -0,0 +1,19 @@
+import { RunFunction } from '../../interfaces/Command';
+export const name: string = 'deleteuserdata';
+export const run: RunFunction = async (client, message) => {
+	const GuildConfigSchema = await client.db.load('usereconomy');
+	const Delete = await GuildConfigSchema.delete({ User: message.author.id });
+	return await message.channel.send(
+		client.embed(
+			{
+				description: Delete
+					? 'I deleted the user data.'
+					: 'I did nothing as there was nothing to delete.',
+			},
+			message
+		)
+	);
+};
+export const category: string = 'config';
+export const userPermissions: string = 'MANAGE_GUILD';
+export const aliases: string[] = ['deluserdata'];
---
+++ src/models/User Models/UserEconomy.ts
@@ -0,0 +1,9 @@
+import { model, Schema } from 'mongoose';
+export const name: string = 'usereconomy';
+export const data = model(
+	'usereconomy',
+	new Schema({
+		User: String,
+		Coins: Number,
+	})
+);
