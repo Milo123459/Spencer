bc1a9ee5a80c66baf1f992e5feadca049e4fe6d2
Milo123459 <milo5huntington@gmail.com>
2020-11-25 21:17:17 +0000

    feat(Stonks): add stonks command

---
+++ src/commands/Fun Commands/StonksCommand.ts
@@ -0,0 +1,19 @@
+import { RunFunction } from '../../interfaces/Command';
+import * as ImageAPI from 'imageapi.js';
+
+export const run: RunFunction = async (client, message) => {
+	const meme = await ImageAPI.advanced('stonks');
+	await message.channel.send(
+		client
+			.embed(
+				{
+					title: meme.title,
+					description: `Upvotes: 👍 ${meme.upvotes} | u/**${meme.author}** in r/**meme**`,
+				},
+				message
+			)
+			.setImage(meme.img)
+	);
+};
+export const name: string = 'stonks';
+export const category: string = 'fun';
