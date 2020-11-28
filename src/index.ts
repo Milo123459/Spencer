d9165b1940cb1e93e1156eb4c1e7eca7fb3c6943
Milo123459 <milo5huntington@gmail.com>
2020-11-28 09:01:46 +0000

    fix(BotConfig): uhard code it

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { Config } from './interfaces/Config';
 import * as File from '../config.json';
 const BotConfig: Config = File;
 import { Spencer } from './client/Client';
-new Spencer().start({ ...BotConfig, prefix: 'test' });
+new Spencer().start(BotConfig);
