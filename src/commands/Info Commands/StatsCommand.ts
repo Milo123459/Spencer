5b0279f9cb5b3572530d0dceca39f11699c35e5e
Milo123459 <milo5huntington@gmail.com>
2020-11-29 10:11:37 +0000

    feat(Stats): update stats command

--- src/commands/Info Commands/StatsCommand.ts
+++ src/commands/Info Commands/StatsCommand.ts
@@ -4,9 +4,14 @@ export const name: string = 'stats';
 export const run: RunFunction = async (client, message) => {
 	const embed: MessageEmbed = client.embed(
 		{
-			description: `Stats for Spencer!\n*Spencer is open source, you can view the code [here](https://github.com/Milo123459/Spencer) - Contributions are welcome!*\nGuilds: \`${client.guilds.cache.size.toLocaleString()}\`\nUsers: \`${client.users.cache.size.toLocaleString()}\`\nChannels: \`${client.channels.cache.size.toLocaleString()}\`\nUptime: \`${client.utils.formatMS(
-				client.uptime
-			)}\``,
+			description: `
+			Stats for Spencer!
+			*Spencer is open source, you can view the code [here](https://github.com/Milo123459/Spencer) - Contributions are welcome!*
+			Guilds: \`${client.guilds.cache.size.toLocaleString()}\`
+			Users: \`${client.users.cache.size.toLocaleString()}\`
+			Channels: \`${client.channels.cache.size.toLocaleString()}\`
+			Uptime: \`${client.utils.formatMS(client.uptime)}\`
+			Discord API version: \`${client.options.http?.version || 'Unkown'}\``,
 		},
 		message
 	);
