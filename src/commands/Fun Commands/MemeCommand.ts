cec6f721a02d257c0a95b94fadb488dd17457114
Milo123459 <milo5huntington@gmail.com>
2020-12-07 21:13:04 +0000

    chore(Lint): lint

--- src/commands/Fun Commands/MemeCommand.ts
+++ src/commands/Fun Commands/MemeCommand.ts
@@ -3,8 +3,10 @@ import { Util } from 'discord.js';
 import * as ImageAPI from 'imageapi.js';
 
 export const run: RunFunction = async (client, message) => {
-	const memeSubreddits = ['meme', 'memes', 'dankmemes']
-	const meme = await ImageAPI.advanced(memeSubreddits[Math.floor(Math.random()* memeSubreddits.length)]);
+	const memeSubreddits = ['meme', 'memes', 'dankmemes'];
+	const meme = await ImageAPI.advanced(
+		memeSubreddits[Math.floor(Math.random() * memeSubreddits.length)]
+	);
 	await message.channel.send(
 		client
 			.embed(
