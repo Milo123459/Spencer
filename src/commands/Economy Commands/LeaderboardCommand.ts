d009c02806f6bb5fe31681e90b562f0d95c9b50b
Milo123459 <milo5huntington@gmail.com>
2020-12-12 20:55:05 +0000

    feat(LeaderboardCommand): display your position

--- src/commands/Economy Commands/LeaderboardCommand.ts
+++ src/commands/Economy Commands/LeaderboardCommand.ts
@@ -1,21 +1,32 @@
 import { RunFunction } from '../../interfaces/Command';
 import { Anything } from '../../interfaces/Anything';
 import { Document } from 'mongoose';
+import { Util } from 'discord.js';
+
 export const run: RunFunction = async (client, message) => {
 	const EconomySchema = await client.db.load('usereconomy');
 	const Leaderboard = await EconomySchema.leaderboard(
 		(a: Anything, b: Anything) => b.Coins - a.Coins
 	);
+	const you: Document = Leaderboard.filter(
+		(value: Document) => (value as Anything).User == message.author.id
+	)[0];
 	await message.channel.send(
 		client.embed(
 			{
 				description: `${Leaderboard.map(
 					(value: Document, index: number) =>
-						`${index + 1} - ${
+						`${index + 1} - ${Util.escapeMarkdown(
 							client.users.cache.get((value as Anything).User)?.tag ||
-							'Unkown user'
-						} - \`${(value as Anything).Coins.toLocaleString()} coins.\``
-				).join('\n')}`,
+								'Unknown user'
+						)} - \`${(value as Anything).Coins.toLocaleString()} coins.\``
+				).join(
+					'\n'
+				)}\n\nYou - ${Leaderboard.map((value: Document, index: number) =>
+					(value as Anything).User == message.author.id ? index + 1 : 0
+				).filter(
+					(value: number) => value != 0
+				)} - \`${(you as Anything).Coins.toLocaleString()} coins.\``,
 			},
 			message
 		)
