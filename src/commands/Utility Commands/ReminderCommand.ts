df1ec2f3f37f144e3f2c2f8f6d23f334b6e6e7f2
Milo123459 <milo5huntington@gmail.com>
2020-12-12 10:01:37 +0000

    feat(ReminderSystem): add reminder system

---
+++ src/commands/Utility Commands/ReminderCommand.ts
@@ -0,0 +1,50 @@
+import { RunFunction } from '../../interfaces/Command';
+import ms from 'ms';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return message.channel.send(
+			client.embed({ description: 'Please specify a time!' }, message)
+		);
+	if (!args[1])
+		return message.channel.send(
+			client.embed({ description: 'Please specify a message!' }, message)
+		);
+	let time: number;
+	time = ms(args[0]);
+	if (isNaN(time)) time = parseInt(ms(time));
+	if (!time)
+		return message.channel.send(
+			client.embed(
+				{
+					description:
+						"That number didn't go through correctly, try again with a valid time!",
+				},
+				message
+			)
+		);
+	if (time > ms('30d'))
+		return message.channel.send(
+			client.embed(
+				{ description: "You can't set a reminder for more then 30 days!" },
+				message
+			)
+		);
+	const ReminderSchema = await client.db.load('reminder');
+	await ReminderSchema.create({
+		User: message.author.id,
+		Message: args.slice(1).join(' '),
+		Time: Date.now() + time,
+	});
+	await message.channel.send(
+		client.embed(
+			{
+				description:
+					"Done, please make sure you're DMs are open! Note: Reminders are checked every minute, so if you set something less then a minute, you may be waiting a bit longer!",
+			},
+			message
+		)
+	);
+};
+export const name: string = 'reminder';
+export const category: string = 'utility';
--- src/events/Client Events/ReadyEvent.ts
+++ src/events/Client Events/ReadyEvent.ts
@@ -1,8 +1,26 @@
 import { RunFunction } from '../../interfaces/Event';
+import { Document } from 'mongoose';
+import { Anything } from '../../interfaces/Anything';
+import { User } from 'discord.js';
+
 export const name: string = 'ready';
 export const run: RunFunction = async (client) => {
 	client.logger.success(`${client.user.tag} is now online!`);
 	await client.user.setActivity(`${client.prefix}help | 👦 Spencer`, {
 		type: 'WATCHING',
 	});
+	setInterval(async () => {
+		const ReminderSchema = await client.db.load('reminder');
+		(await ReminderSchema.find({})).map(async (value: Document) => {
+			if (Date.now() >= (value as Anything).Time) {
+				const User: User = client.users.cache.get((value as Anything).User);
+				try {
+					await value.delete();
+					await User.send(
+						`I was told to remind you: ${(value as Anything).Message}`
+					);
+				} catch {}
+			}
+		});
+	}, 1000 * 60);
 };
