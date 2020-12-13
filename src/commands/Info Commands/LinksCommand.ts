1b4945b559f6d7359dea39ca44fb83bd83bfd996
Milo123459 <milo5huntington@gmail.com>
2020-12-13 12:35:13 +0000

    feat(LinksCommand): add donatebot link

--- src/commands/Info Commands/LinksCommand.ts
+++ src/commands/Info Commands/LinksCommand.ts
@@ -7,7 +7,7 @@ export const run: RunFunction = async (client, message) => {
 				description: `
     You can invite the bot [here](https://discord.com/api/oauth2/authorize?client_id=765156777607823380&permissions=8&scope=bot) 
 	You can join the support server [here](https://discord.gg/22TtDpJcNE)
-	You can vote for ${client.user.username} on top.gg [here](https://top.gg/bot/${client.user.id}/vote), on weekdays you get 1000 coins, on weekends you get 2000!`.trim(),
+	You can vote for ${client.user.username} on top.gg [here](https://top.gg/bot/${client.user.id}/vote), on weekdays you get 1000 coins, on weekends you get 2000!\nYou can donate to Spencer [here](https://donatebot.io/checkout/784470505607528448) - if you join Spencer Support you can even get special roles!`.trim(),
 			},
 			message
 		)
