f1c557165f5d1d42db9552059885ed8bcc02a953
Milo123459 <milo5huntington@gmail.com>
2020-12-17 11:36:52 +0000

    feat(Directories): update some directories

--- src/commands/Economy Commands/SetJobCommand.ts
+++ src/commands/Economy Commands/SetJobCommand.ts
@@ -1,54 +1,17 @@
 import { Message } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
-
-interface Option {
-	reaction: string;
-	name: string;
-	description: string;
-	n?: boolean;
-}
+import { Job } from '../../interfaces/Job';
+import { Jobs } from '../../static/Jobs';
 
 export const run: RunFunction = async (client, message) => {
 	const EconomyShcmea = await client.db.load('usereconomy');
-	const options: Array<Option> = [
-		{
-			reaction: '⚽',
-			name: 'Footballer',
-			description: `Work as a footabller. Fun right?`,
-		},
-		{
-			reaction: '💻',
-			name: 'Programmer',
-			description: 'Work as a epic gamer chad programmer guy!!!',
-		},
-		{
-			reaction: '🎥',
-			name: 'Movie director',
-			description: "You make movies, just, don't look at the reviews.",
-		},
-		{
-			reaction: '🚀',
-			name: 'Rocket scientist',
-			description: "It's not rocket science to be a rocket scientist.",
-		},
-		{
-			reaction: '🚗',
-			name: 'Uber driver',
-			description:
-				"I'm going to charge you waiting time because I couldn't find your house.",
-			n: true,
-		},
-	];
-
 	const msg: Message = await message.channel.send(
 		client.embed(
 			{
-				description: `Please pick a job by reacting below.\n${options
-					.map(
-						(value: Option) =>
-							`${value.reaction} **${value.name}**: ${value.description}`
-					)
-					.join('\n')}`,
+				description: `Please pick a job by reacting below.\n${Jobs.map(
+					(value: Job) =>
+						`${value.reaction} **${value.name}**: ${value.description}`
+				).join('\n')}`,
 			},
 			message
 		)
@@ -57,22 +20,20 @@ export const run: RunFunction = async (client, message) => {
 		const reaction: string = await client.utils.awaitReactions(
 			message.author.id,
 			msg,
-			options.map((opt: Option) => opt.reaction)
+			Jobs.map((opt: Job) => opt.reaction)
 		);
 		await EconomyShcmea.update(
 			{ User: message.author.id },
-			{ Job: options.find((value: Option) => value.reaction == reaction).name }
+			{ Job: Jobs.find((value: Job) => value.reaction == reaction).name }
 		);
 		await msg.edit(
 			client.embed(
 				{
 					description: `Woohoo! You\'re now a${
-						options.find((value: Option) => value.reaction == reaction)?.n
-							? 'n'
-							: ''
-					} **${options
-						.find((value: Option) => value.reaction == reaction)
-						.name.toLowerCase()}**`,
+						Jobs.find((value: Job) => value.reaction == reaction)?.n ? 'n' : ''
+					} **${Jobs.find(
+						(value: Job) => value.reaction == reaction
+					).name.toLowerCase()}**`,
 				},
 				message
 			)
---
+++ src/static/Jobs.ts
@@ -0,0 +1,31 @@
+import { Job } from '../interfaces/Job';
+
+export const Jobs: Array<Job> = [
+	{
+		reaction: '⚽',
+		name: 'Footballer',
+		description: `Work as a footabller. Fun right?`,
+	},
+	{
+		reaction: '💻',
+		name: 'Programmer',
+		description: 'Work as a epic gamer chad programmer guy!!!',
+	},
+	{
+		reaction: '🎥',
+		name: 'Movie director',
+		description: "You make movies, just, don't look at the reviews.",
+	},
+	{
+		reaction: '🚀',
+		name: 'Rocket scientist',
+		description: "It's not rocket science to be a rocket scientist.",
+	},
+	{
+		reaction: '🚗',
+		name: 'Uber driver',
+		description:
+			"I'm going to charge you waiting time because I couldn't find your house.",
+		n: true,
+	},
+];
