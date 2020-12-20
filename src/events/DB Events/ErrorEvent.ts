1ba97a3f899c24414ebba3c272c9bd3d74f04135
Milo123459 <milo5huntington@gmail.com>
2020-12-20 13:18:52 +0000

    fix(ErrorEvent): fix temp. literal

--- src/events/DB Events/ErrorEvent.ts
+++ src/events/DB Events/ErrorEvent.ts
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 import { EventEmitter } from 'events';
 export const name: string = 'error';
 export const run: RunFunction = async (client, error: unknown) => {
-	client.logger.error(`DB: error`);
+	client.logger.error(`DB: ${error}`);
 };
 export const emitter: EventEmitter = mongoose.connection;
