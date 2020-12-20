71bdbfe66d6ab936de7f273f774862f41a144743
Fionn <fionn@fionn.cc>
2020-12-20 17:51:21 +0000

    Added Dadbot Command!

---
+++ src/commands/Fun Commands/DadBotCommand.ts
@@ -0,0 +1,19 @@
+import { RunFunction } from '../../interfaces/Command';
+import fetch from 'node-fetch';
+
+export const run: RunFunction = async (client, message) => {
+	fetch('https://icanhazdadjoke.com/', {
+		headers: {
+			Accept: 'application/json',
+		},
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			message.channel.send(
+				client.embed({ title: 'Dad Joke!', description: res.joke }, message)
+			);
+		});
+};
+export const name: string = 'dadjoke';
+export const category: string = 'fun';
+export const cooldown: number = 3000;
