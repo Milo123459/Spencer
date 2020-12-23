79d184531582d0137c8b5c3435b01714dc73255e
Milo123459 <milo5huntington@gmail.com>
2020-12-23 14:40:34 +0000

    feat(MembersCommand): add membercount command

---
+++ src/commands/Info Commands/MembersCommand.ts
@@ -0,0 +1,15 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message) => {
+	return await message.channel.send(
+		client.embed(
+			{
+				description: `**${message.guild.name}** has got **${message.guild.memberCount}** members! (Well, cached members!)`,
+			},
+			message
+		)
+	);
+};
+export const aliases: string[] = ['membercount'];
+export const name: string = 'members';
+export const category: string = 'info';
