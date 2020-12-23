3eb855bb3c29734fd2a5d0079ca749b4ccc1ab47
Milo123459 <milo5huntington@gmail.com>
2020-12-23 14:08:06 +0000

    feat(FunCommands): add lenny and clap command

---
+++ src/commands/Fun Commands/ClapCommand.ts
@@ -0,0 +1,14 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return await message.channel.send(
+			client.embed({ description: 'Please provide a message!' }, message)
+		);
+	await message.channel.send(
+		client.embed({ description: args.join(' 👏 ') }, message)
+	);
+};
+export const name: string = 'clap';
+export const category: string = 'fun';
+export const aliases: string[] = ['clapify'];
---
+++ src/commands/Fun Commands/LennyCommand.ts
@@ -0,0 +1,14 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return await message.channel.send(
+			client.embed({ description: 'Please provide a message!' }, message)
+		);
+	await message.channel.send(
+		client.embed({ description: args.join(' ( ͡° ͜ʖ ͡°) ') }, message)
+	);
+};
+export const name: string = 'lenny';
+export const category: string = 'fun';
+export const aliases: string[] = ['lennyify'];
