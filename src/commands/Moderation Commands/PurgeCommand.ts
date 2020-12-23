e923cc4f40562f324ff12d40cba9ab19f6860d23
Milo123459 <milo5huntington@gmail.com>
2020-12-23 13:58:24 +0000

    feat(PurgeCommand): add purge command

---
+++ src/commands/Moderation Commands/PurgeCommand.ts
@@ -0,0 +1,33 @@
+import { TextChannel } from 'discord.js';
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return await message.channel.send(
+			client.embed(
+				{
+					description:
+						"Please specify how many messages you'd like to purge, 1-100!",
+				},
+				message
+			)
+		);
+	if (
+		isNaN(parseInt(args[0])) ||
+		parseInt(args[0]) < 0 ||
+		parseInt(args[0]) > 100
+	)
+		return await message.channel.send(
+			client.embed(
+				{
+					description:
+						'Please provide a valid number & a number that is between 1-100!',
+				},
+				message
+			)
+		);
+	await message.delete();
+	await (message.channel as TextChannel).bulkDelete(parseInt(args[0]));
+};
+export const name: string = 'purge';
+export const category: string = 'moderation';
