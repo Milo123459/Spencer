53319c9124957de9761116a4f8426de253095f38
Milo123459 <milo5huntington@gmail.com>
2020-12-24 22:01:35 +0000

    feat(PingCommand): remove stupidity

--- src/commands/Info Commands/PingCommand.ts
+++ src/commands/Info Commands/PingCommand.ts
@@ -6,13 +6,10 @@ export const run: RunFunction = async (client, message) => {
 	);
 	const ws: number = client.ws.ping;
 	const edit: number = msg.createdTimestamp - message.createdTimestamp;
-	const pingStatus = client.utils.pingStatus(ws, edit);
 	await msg.edit(
 		client.embed(
 			{
-				description: `WebSocket ping: \`${ws}\`MS, API Ping: \`${edit}\`MS\n\nStatus:\nWS: ${
-					pingStatus.ws ? 'Average' : 'Not average'
-				}\nEdit: ${pingStatus.edit ? 'Average' : 'Not average'}`,
+				description: `WebSocket ping: \`${ws}\`MS, API Ping: \`${edit}\`MS`,
 			},
 			message
 		)
