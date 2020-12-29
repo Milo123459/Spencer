e50c9cf7d2d684571d067b9231370f06e45c7d81
Beatzoid <epiccoder7@gmail.com>
2020-12-28 18:09:24 -0800

    feat(WaterCommand): add water command

---
+++ src/commands/Fun Commands/WaterCommand.ts
@@ -0,0 +1,18 @@
+import { MessageAttachment } from 'discord.js';
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return message.channel.send(
+			'I need some text to generate this meme! `water <text>`'
+		);
+	const buffer = await client.vacefron.water(args.join(' '));
+	const attachment = new MessageAttachment(buffer);
+
+	return message.channel.send(attachment);
+};
+
+export const name: string = 'water';
+export const category: string = 'fun';
+export const description: string =
+	'Generate a water meme. (Example: https://i.imgur.com/x2p25U6.png)';
