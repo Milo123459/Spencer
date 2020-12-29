d58c79550edf45d64ded53aba2b1f9b00f86986b
Beatzoid <epiccoder7@gmail.com>
2020-12-29 08:50:29 -0800

    feat(JsDocCommand): add jsdoc command

---
+++ src/commands/Info Commands/JsDocCommand.ts
@@ -0,0 +1,47 @@
+import { MessageEmbed } from 'discord.js';
+import nodeFetch from 'node-fetch';
+import { RunFunction } from '../../interfaces/Command';
+import paginationEmbed from 'discord.js-pagination';
+
+export const run: RunFunction = async (client, message, args) => {
+	if (!args.length)
+		return message.channel.send(
+			'Invalid usage, provide a search term! `jsdoc <search input>`'
+		);
+
+	const pages = [];
+
+	nodeFetch(
+		`https://developer.mozilla.org/api/v1/search/en-US?q=${args.join(
+			' '
+		)}&locale=en-US&highlight=false`
+	)
+		.then((body) => body.json())
+		.then(async (result) => {
+			result.documents.forEach((document) => {
+				if (!document.slug.toLowerCase().includes('javascript')) {
+					return;
+				}
+
+				pages.push(
+					new MessageEmbed()
+						.setTitle(document.title)
+						.setAuthor(document.slug)
+						.setDescription(document.excerpt.replace(/<\/?[^>]+(>|$)/g, ''))
+						.setColor('#2F3136')
+				);
+			});
+
+			if (!pages.length) {
+				return message.channel.send(
+					client.embed({ title: 'No results!' }, message)
+				);
+			}
+
+			paginationEmbed(message, pages, ['⏪', '⏩'], 30000);
+		});
+};
+
+export const name: string = 'jsdoc';
+export const category: string = 'info';
+export const description: string = 'Search for something on the MDN JS Docs';
