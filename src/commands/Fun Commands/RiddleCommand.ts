defb84b9fd9fb82501131b62c7e909134941a99b
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2020-12-28 17:29:39 -0800

    feat(RiddleCommand): change description

--- src/commands/Fun Commands/RiddleCommand.ts
+++ src/commands/Fun Commands/RiddleCommand.ts
@@ -15,4 +15,4 @@ export const run: RunFunction = async (client, message, args) => {
 
 export const name: string = 'riddle';
 export const category: string = 'fun';
-export const description: string = 'Fetch a random fact and answer from an API.';
+export const description: string = 'Get a random riddle and answer!';
