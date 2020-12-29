9f3e3b4e246f1a59671ff861458b2e6ca3d11213
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2020-12-28 21:53:06 -0800

    feat(Roll and Flip Commands): add roll and flip command

---
+++ src/commands/Fun Commands/RollCommand.ts
@@ -0,0 +1,19 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async (client, message, args) => {
+	const randomRoll = Math.floor(Math.random() * 6) + 1;
+	message.channel.send(
+		client.embed(
+			{
+				title: '🎲 Roll! 🎲',
+				description: `The dice rolled a ${randomRoll}!`,
+			},
+			message
+		)
+	);
+};
+
+export const name: string = 'roll';
+export const category: string = 'fun';
+export const description: string = 'Roll a dice!';
+export const aliases: string[] = ['rolldice', 'rolldie'];
