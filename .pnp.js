68b20c39632c4c6a3591fef7e56ef3c73219ca53
Milo123459 <milo5huntington@gmail.com>
2021-01-04 01:18:36 +0000

    chore(Deps): bump deps

--- .pnp.js
+++ .pnp.js
@@ -40,7 +40,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@commitlint/cli", "npm:11.0.0"],
             ["@commitlint/config-angular", "npm:11.0.0"],
             ["@pm2/io", "npm:4.3.5"],
-            ["@top-gg/sdk", "npm:3.0.5"],
+            ["@top-gg/sdk", "npm:3.0.6"],
             ["@types/express", "npm:4.17.9"],
             ["@types/glob", "npm:7.1.3"],
             ["@types/mathjs", "npm:6.0.9"],
@@ -53,7 +53,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@yarnpkg/doctor", "npm:2.1.3"],
             ["@yarnpkg/pnpify", "virtual:523c71c97f9702a89e93eae878f1287b4eb8704861aa07c213e341a8d0f4bd2964affb313176b35b904ddeb0fe1bbca462caa20832357c33b9732b9e87933daf#npm:2.4.0"],
             ["alpha-sort", "npm:4.0.0"],
-            ["bufferutil", "npm:4.0.2"],
+            ["bufferutil", "npm:4.0.3"],
             ["cleverbot-free", "npm:1.0.5"],
             ["consola", "npm:2.15.0"],
             ["del-cli", "npm:3.0.1"],
@@ -74,7 +74,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["ts-node-dev", "virtual:523c71c97f9702a89e93eae878f1287b4eb8704861aa07c213e341a8d0f4bd2964affb313176b35b904ddeb0fe1bbca462caa20832357c33b9732b9e87933daf#npm:1.1.1"],
             ["tslib", "npm:2.0.3"],
             ["typescript", "patch:typescript@npm%3A4.1.3#builtin<compat/typescript>::version=4.1.3&hash=cc6730"],
-            ["utf-8-validate", "npm:5.0.3"],
+            ["utf-8-validate", "npm:5.0.4"],
             ["vacefron", "npm:3.2.0"],
             ["yn", "npm:4.0.0"],
             ["zlib-sync", "npm:0.1.7"]
@@ -472,10 +472,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@top-gg/sdk", [
-        ["npm:3.0.5", {
-          "packageLocation": "./.yarn/cache/@top-gg-sdk-npm-3.0.5-190a363779-111a603354.zip/node_modules/@top-gg/sdk/",
+        ["npm:3.0.6", {
+          "packageLocation": "./.yarn/cache/@top-gg-sdk-npm-3.0.6-08177e102c-2dc3ff882c.zip/node_modules/@top-gg/sdk/",
           "packageDependencies": [
-            ["@top-gg/sdk", "npm:3.0.5"],
+            ["@top-gg/sdk", "npm:3.0.6"],
             ["node-fetch", "npm:2.6.1"],
             ["raw-body", "npm:2.4.1"]
           ],
@@ -2041,10 +2041,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["bufferutil", [
-        ["npm:4.0.2", {
-          "packageLocation": "./.yarn/unplugged/bufferutil-npm-4.0.2-6f283689b1/node_modules/bufferutil/",
+        ["npm:4.0.3", {
+          "packageLocation": "./.yarn/unplugged/bufferutil-npm-4.0.3-ba64aa1a24/node_modules/bufferutil/",
           "packageDependencies": [
-            ["bufferutil", "npm:4.0.2"],
+            ["bufferutil", "npm:4.0.3"],
             ["node-gyp", "npm:7.1.2"],
             ["node-gyp-build", "npm:4.2.3"]
           ],
@@ -6803,7 +6803,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@commitlint/cli", "npm:11.0.0"],
             ["@commitlint/config-angular", "npm:11.0.0"],
             ["@pm2/io", "npm:4.3.5"],
-            ["@top-gg/sdk", "npm:3.0.5"],
+            ["@top-gg/sdk", "npm:3.0.6"],
             ["@types/express", "npm:4.17.9"],
             ["@types/glob", "npm:7.1.3"],
             ["@types/mathjs", "npm:6.0.9"],
@@ -6816,7 +6816,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@yarnpkg/doctor", "npm:2.1.3"],
             ["@yarnpkg/pnpify", "virtual:523c71c97f9702a89e93eae878f1287b4eb8704861aa07c213e341a8d0f4bd2964affb313176b35b904ddeb0fe1bbca462caa20832357c33b9732b9e87933daf#npm:2.4.0"],
             ["alpha-sort", "npm:4.0.0"],
-            ["bufferutil", "npm:4.0.2"],
+            ["bufferutil", "npm:4.0.3"],
             ["cleverbot-free", "npm:1.0.5"],
             ["consola", "npm:2.15.0"],
             ["del-cli", "npm:3.0.1"],
@@ -6837,7 +6837,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["ts-node-dev", "virtual:523c71c97f9702a89e93eae878f1287b4eb8704861aa07c213e341a8d0f4bd2964affb313176b35b904ddeb0fe1bbca462caa20832357c33b9732b9e87933daf#npm:1.1.1"],
             ["tslib", "npm:2.0.3"],
             ["typescript", "patch:typescript@npm%3A4.1.3#builtin<compat/typescript>::version=4.1.3&hash=cc6730"],
-            ["utf-8-validate", "npm:5.0.3"],
+            ["utf-8-validate", "npm:5.0.4"],
             ["vacefron", "npm:3.2.0"],
             ["yn", "npm:4.0.0"],
             ["zlib-sync", "npm:0.1.7"]
@@ -7564,10 +7564,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["utf-8-validate", [
-        ["npm:5.0.3", {
-          "packageLocation": "./.yarn/unplugged/utf-8-validate-npm-5.0.3-70d3f814e3/node_modules/utf-8-validate/",
+        ["npm:5.0.4", {
+          "packageLocation": "./.yarn/unplugged/utf-8-validate-npm-5.0.4-29bc22db06/node_modules/utf-8-validate/",
           "packageDependencies": [
-            ["utf-8-validate", "npm:5.0.3"],
+            ["utf-8-validate", "npm:5.0.4"],
             ["node-gyp", "npm:7.1.2"],
             ["node-gyp-build", "npm:4.2.3"]
           ],
