b2eba105537bd2fcc1b883a3959e4ed39bbc397b
Milo123459 <milo5huntington@gmail.com>
2021-01-04 16:40:01 +0000

    feat(Events): remove un-used events

--- src/events/Guild Events/GuildMemberAddEvent.ts
+++
@@ -1,5 +0,0 @@
-import { GuildMember } from 'discord.js';
-import { RunFunction } from '../../interfaces/Event';
-export const name: string = 'guildMemberAdd';
-export const run: RunFunction = async (client, member: GuildMember) =>
-	member.fetch(true);
