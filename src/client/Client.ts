9ea67ef4926fb0db6f7dbcd6dcfcb6f94626757c
Milo123459 <milo5huntington@gmail.com>
2021-01-17 09:46:25 +0000

    feat(Pm2): remove metrics

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -10,7 +10,6 @@ import {
 import { DatabaseManager } from '../db/Database';
 import { UtilsManager } from '../utils/Utils';
 import glob from 'glob';
-import io from '@pm2/io';
 import { promisify } from 'util';
 import mongoose from 'mongoose';
 import { Command } from '../interfaces/Command';
@@ -35,12 +34,6 @@ class Spencer extends Client {
 	public owners: Array<string>;
 	public config: Config;
 	public vacefron: VACEFronJS = new VACEFronJS();
-	public pm2 = io.init();
-	public guildconfigMetric = this.pm2.metric({ name: 'guildconfigs' });
-	public raiduserMetric = this.pm2.metric({ name: 'raidusers' });
-	public reminderMetric = this.pm2.metric({ name: 'reminders' });
-	public suggestionMetric = this.pm2.metric({ name: 'suggestions' });
-	public usereconomyMetric = this.pm2.metric({ name: 'usereconomies' });
 	public topGGApi: Api;
 	public topGGWebhook: Webhook;
 	public constructor() {
