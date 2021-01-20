67fb9095a121119f53a7051cdac0dc55550ca970
Milo123459 <milo5huntington@gmail.com>
2021-01-20 11:34:30 +0000

    feat(Inventory): allow you to get other peoples inventories

--- src/commands/Economy Commands/InventoryCommand.ts
+++ src/commands/Economy Commands/InventoryCommand.ts
@@ -5,9 +5,11 @@ import { Item } from '../../interfaces/Item';
 import pagination from '../../utils/Pagination';
 import { limitArray } from 'tyvn';
 
-export const run: RunFunction = async (client, message) => {
+export const run: RunFunction = async (client, message, args) => {
 	const UserEconomy = await client.db.load('usereconomy');
-	const Profile = await UserEconomy.findOne({ User: message.author.id });
+	const User =
+		client.utils.ResolveMember(message, args[0])?.id || message.author.id;
+	const Profile = await UserEconomy.findOne({ User: User });
 	const Inventory = (Profile as Anything)?.Inventory || {};
 
 	return pagination(
@@ -20,7 +22,9 @@ export const run: RunFunction = async (client, message) => {
 							name: `${
 								items.find((value_: Item) => value_.id == value[0]).name
 							}`,
-							value: `You have ${value[1]} ${items
+							value: `${User == message.author.id ? 'You have' : 'They have'} ${
+								value[1]
+							} ${items
 								.find((value_: Item) => value_.id == value[0])
 								.name.toLowerCase()}s.`,
 						};
@@ -38,3 +42,4 @@ export const name: string = 'inventory';
 export const category: string = 'economy';
 export const description: string = 'View your inventory';
 export const aliases: string[] = ['inv'];
+export const usage: string = '[user]';
