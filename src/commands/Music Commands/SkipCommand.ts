4fd8883cfccde391b8ed3113d808c86fc395867c
Milo123459 <milo5huntington@gmail.com>
2021-01-25 08:37:25 +0000

    feat(Skip): it works

--- src/commands/Music Commands/SkipCommand.ts
+++ src/commands/Music Commands/SkipCommand.ts
@@ -1,6 +1,6 @@
 import { RunFunction } from '../../interfaces/Command';
 
-export const run: RunFunction = async (client, message, args) => {
+export const run: RunFunction = async (client, message) => {
 	if (!client.utils.checkVC(message)) {
 		return message.channel.send(
 			client.embed(
@@ -10,6 +10,46 @@ export const run: RunFunction = async (client, message, args) => {
 				},
 				message
 			)
+		);
+	}
+	const dispatch = client.music.dispatchers.get(message.guild.id);
+	if (!dispatch)
+		return message.channel.send(
+			client.embed({ description: 'I am not playing music!' }, message)
+		);
+	if (!message.guild.me.voice?.channel)
+		return message.channel.send(
+			client.embed({ description: 'I need to be in a VC as well!' }, message)
+		);
+	if (!dispatch.queue.size)
+		return message.channel.send(
+			client.embed(
+				{ description: 'There is nothing else in the queue!' },
+				message
+			)
+		);
+	if (
+		message.guild.me.voice.channel.members.size == 2 &&
+		message.guild.me.voice.channel.members.has(message.author.id)
+	) {
+		await dispatch.player.stopTrack();
+		return message.channel.send(
+			client.embed({ description: 'Skipped!' }, message)
+		);
+	} else {
+		if (!message.member.permissions.has('MANAGE_GUILD'))
+			return message.channel.send(
+				client.embed(
+					{
+						description:
+							'You need MANAGE_GUILD to skip as there are other people in the VC.',
+					},
+					message
+				)
+			);
+		await dispatch.player.stopTrack();
+		return message.channel.send(
+			client.embed({ description: 'Skipped!' }, message)
 		);
 	}
 };
