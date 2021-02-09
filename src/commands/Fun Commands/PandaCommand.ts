8e4f5bec11e1ebb24965bd21c1982ed8afd5645d
Milo123459 <milo5huntington@gmail.com>
2021-02-09 12:00:01 +0000

    feat(Panda): add panda command

--- src/commands/Fun Commands/PandaCommand.ts
+++ src/commands/Fun Commands/PandaCommand.ts
@@ -3,13 +3,13 @@ import { SRAImage } from '../../interfaces/SRAImage';
 import fetch from 'node-fetch';
 export const run: RunFunction = async (client, message) => {
 	const response = (await fetch(
-		'https://some-random-api.ml/img/red_panda'
+		'https://some-random-api.ml/img/panda'
 	).then((res) => res.json())) as SRAImage;
 	return message.channel.send(
 		client.embed({ image: { url: response.link } }, message)
 	);
 };
-export const name: string = 'redpanda';
+export const name: string = 'panda';
 export const category: string = 'fun';
-export const description: string = 'Get a random red panda image!';
+export const description: string = 'Get a random panda image!';
 export const cooldown: number = 5000;
