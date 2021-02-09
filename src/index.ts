572bd322a7f31eb88da865aed2a00ec0d4aee4a7
Milo123459 <milo5huntington@gmail.com>
2021-02-09 11:11:46 +0000

    feat(Index): ts-ignore

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { Config } from './interfaces/Config';
+// @ts-ignore
 import * as File from '../config.json';
 const BotConfig: Config = File;
 import { Spencer } from './client/Client';
 new Spencer().start(BotConfig);
-
-/*
-Spencer
-*/
