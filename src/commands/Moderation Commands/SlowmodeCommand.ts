8d24c47c080548516d3d984b41a45d55a10e8901
Milo123459 <milo5huntington@gmail.com>
2021-02-12 11:11:01 +0000

    feat(Slowmode): allow you to clear slowmode

--- src/commands/Moderation Commands/SlowmodeCommand.ts
+++ src/commands/Moderation Commands/SlowmodeCommand.ts
@@ -6,20 +6,23 @@ export const run: RunFunction = async (client, message, args) => {
 		return message.channel.send(
 			client.embed({ description: 'Please specify a time!' }, message)
 		);
+	let clear: boolean = false;
+	if (args[0].toLowerCase() == 'clear') clear = true;
+	console.log(clear);
 	let time: number;
 	time = ms(args[0]);
 	if (!time) time = parseInt(ms(time || 0), 10);
-	if (!time || isNaN(time))
+	if ((!time || isNaN(time)) && clear == false)
 		return message.channel.send(
 			client.embed(
 				{
 					description:
-						"That number didn't go through correctly, try again with a valid time! The correct usage is <time> <...message>",
+						"That number didn't go through correctly, try again with a valid time! The correct usage is <time | clear>",
 				},
 				message
 			)
 		);
-	if (!time)
+	if (!time && clear == false)
 		return message.channel.send(
 			client.embed(
 				{
@@ -29,7 +32,7 @@ export const run: RunFunction = async (client, message, args) => {
 				message
 			)
 		);
-	if (time > ms('6h') || ms('1s') > time)
+	if ((time > ms('6h') || ms('1s') > time) && clear == false)
 		return message.channel.send(
 			client.embed(
 				{
@@ -45,7 +48,7 @@ export const run: RunFunction = async (client, message, args) => {
 			client.embed({ description: 'I need MANAGE_CHANNELS.' }, message)
 		);
 	await (message.channel as TextChannel).setRateLimitPerUser(
-		Math.floor(time / 1000)
+		clear == true ? 0 : Math.floor(time / 1000)
 	);
 	return message.react('🚀');
 };
@@ -53,3 +56,4 @@ export const name: string = 'slowmode';
 export const category: string = 'moderation';
 export const description: string = 'Set slowmode on a channel';
 export const userPermissions: string[] = ['MANAGE_GUILD'];
+export const usage: string = '<time | clear>';
