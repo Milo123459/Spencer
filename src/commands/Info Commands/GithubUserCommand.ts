07f3338094b943741fe4816935e52acc06ef1e58
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-03-20 19:51:21 +0530

    fix(GithubUserInfo): forgot this isnt moment

--- src/commands/Info Commands/GithubUserCommand.ts
+++ src/commands/Info Commands/GithubUserCommand.ts
@@ -78,18 +78,18 @@ export const run: RunFunction = async (client, message, args) => {
 							},
 							{
 								name: 'Account Creation Date',
-								value: dayjs
-									.utc(res.created_at)
-									.format('dddd Do MMMM, YYYY, hh:mm a'),
+								value: dayjs(res.created_at).format(
+									'dddd Do MMMM, YYYY, hh:mm a'
+								),
 								inline: true,
 							},
 							{
 								name: 'Account Last Updated',
 								value: `${
 									res.updated_at
-										? dayjs
-												.utc(res.updated_at)
-												.format('dddd Do MMMM, YYYY, hh:mm a')
+										? dayjs(res.updated_at).format(
+												'dddd Do MMMM, YYYY, hh:mm a'
+										  )
 										: 'Never Updated'
 								}`,
 								inline: true,
