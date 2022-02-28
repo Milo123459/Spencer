cafdf1d9e0e2f5d1d80b0e27ca63974dc63ed049
Milo <50248166+Milo123459@users.noreply.github.com>
2022-02-28 17:49:09 +0000

    use environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Config } from './interfaces/Config';
-// @ts-ignore
-import * as File from '../config.json';
-const BotConfig: Config = File;
+import { config } from "dotenv"; 
+config();
+const BotConfig: Config = {
+    mongoURI: process.env.MONGO_URI,
+    owners: [process.env.OWNERS],
+    prefix: "sp!",
+    token: process.env.TOKEN,
+};
 import { Spencer } from './client/Client';
 new Spencer().start(BotConfig);
